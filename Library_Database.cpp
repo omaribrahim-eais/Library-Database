e655925b7bfd9a10935745cbcb899b7042b8ff0f
omaribrahim-eais <omaribrahim.eais@gmail.com>
2025-12-20 00:36:14 +0200

    Restore Library_Database.cpp to original state
    Restored the Library_Database.cpp file to its original state, including all functions for managing books and loans.

--- Library_Database.cpp
+++ Library_Database.cpp
@@ -314,7 +314,7 @@ int main() {
     return 0;
 }
 
-//Omar & Kareem
+//Omar & Kareemmmmm
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
@@ -325,4 +325,5 @@ int main() {
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
 
